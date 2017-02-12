a2c542702aca26dc78fa93c8f6e9d8edb5125722
Kark3ys <Kark3ys@users.noreply.github.com>
2017-02-12 01:12:20 +0000

    Finished first draft of WeetStore, testing yet to commence.

--- UserStore.java
+++ UserStore.java
@@ -57,203 +57,219 @@ public class UserStore implements IUserStore {
 	//Hash function is simply X mod 1000000.
 	//This means that users are sorted into bins based on the last 6 digits of
 	//their user id.
-		private int capacity;
-		private UserAndPoint[] hashtable;
-		private int userCount;
-		
-    public UserStore() {
-			capacity = 1000000;
-			hashtable = new UserAndPoint[capacity];
-			userCount = 0;
-			//Array is initialized at null.
-    }
+	private int capacity;
+	private UserAndPoint[] hashtable;
+	private int userCount;
+	
+	public UserStore() {
+		capacity = 1000000;
+		hashtable = new UserAndPoint[capacity];
+		userCount = 0;
+		//Array is initialized at null.
+	}
 
-    public boolean addUser(User usr) {
-			//Time Complexity
-				//Best Case O(1) - Hashcode hasn't been used.
-				//Worst Case O(n) - Every id has the same hashcode.
-			//Space Complexity O(1)
-			int uid = usr.getId();
-			if (getUser(uid) != null) return false;	//Do a check to see if usr in.
-			UserAndPoint temp = hashtable[hashFunction(uid)];
-			UserAndPoint prev = null;
-			//Set up some pointers.
-			while (temp != null) {
-				prev = temp;
-				temp = temp.getNext();
-			}
-			//If the first try wasn't null, keep cycling until it is.
-			if (prev == null) {
-				hashtable[hashFunction(uid)] = new UserAndPoint(usr);
-				//If this is the first value with this hashcode, point directly from
-				//array.
-			} else {
-				prev.gottaPointFast(new UserAndPoint(usr));
-				//If there is more than one user with this hashcode, have the previous 
-				//one point to this one.
-			}
-			
-			this.userCount++;	//Increment users.
-			return true;
-    }
-
-    public User getUser(int uid) {
-			//Time Complexity
-				//Best Case O(1) - Hashcode used once/is unused.
-				//Worst Case O(n) - Every id has the same hashcode.
-			//Space Complexity O(1)
-			UserAndPoint check = hashtable[hashFunction(uid)];
-			while (check != null) {
-				if (check.getCurrent().getId() == uid) return check.getCurrent();
-				check = check.getNext();
-			}
-			return null;
-    }
-
-    public User[] getUsers() {
-			//Time Complexity
-				//Best Case O(n) - No more than one ID to every hashcode.
-				//Worst Case O(n^2) - Every id has the same hashcode=capcity-1.
-			//Space Complexity O(n)
-			if (userCount == 0) return null;
-			User[] retArray = new User[userCount];
-			UserAndPoint temp;
-			int j = 0; //Used for end pointer in retArray
-			for (int i = 0; i < capacity; i++) {
-				if (hashtable[i] != null) {
-					temp = hashtable[i];
-					while (temp != null) {
-						retArray[j] = temp.getCurrent();
-						j++;
-						temp = temp.getNext();
-					}
-				}
-			}
-			//Sort by date joined and return the array.
-			return sortByDJ(retArray);
-    }
-
-    public User[] getUsersContaining(String query) {
-			//Time Complexity O(n)
-			//Space Complexity O(n)
-			User[] arrAllUsers = getUsers();
-			User[] retArray;
-			if (arrAllUsers == null) return null;
-			boolean[] arrMatch = new boolean[arrAllUsers.length]; 
-			//Indexes match with the user indexes in arrAllUsers, so if true in this
-			//array, then it means that user matches the string check.
-			//This round about way is required so we can find the length of the 
-			//return value array, then actually assign the object references without
-			//having to do another set of string checks.
-			int count = 0; //Counter for the number of matches found.
-			int i = 0;
-			for (i = 0; i < arrMatch.length; i++) {
-				arrMatch[i] = arrAllUsers[i].getName().toLowerCase().contains(query.toLowerCase());
-				System.out.println(arrMatch[i]);
-				//Compare the lower case versions of the name and query. If query is
-				//or is part of the current user name, then that index has it's value 
-				//set to true.
-				if (arrMatch[i]) count++;
-			}
-			if (count == 0) return null;
-			retArray = new User[count];
-			int j = 0; //Iterator through retArray.
-			for (i = 0; i < arrMatch.length; i++)
-				if (arrMatch[i]) {
-					retArray[j] = arrAllUsers[i];
-					j++;
-				}
-			//No need to resort the array as we've kept the users in date order, just
-			//shuffled up to fill the gaps.
-			return retArray;
-    }
-
-    public User[] getUsersJoinedBefore(Date dateBefore) {
-			User[] arrAllUsers = getUsers();
-			if (arrAllUsers == null) return null;
-			int count = 0;
-			while (arrAllUsers[count].getDateJoined().compareTo(dateBefore) < 0) count++;
-			//Keep looking through the array until we find the date joined which is
-			//equal to or later than the date provided.
-			if (count == 0) return null;
-			User[] retArray = new User[count];
-			for (int i = 0; i < count; i++) retArray[i] = arrAllUsers[i];
-			//We simply want to truncate the array of the latter parts which contains
-			//users registered after or on the specified date.
-			//As a result sorting isn't needed.
-			return retArray;
-    }
-		
-		private int hashFunction(int uid) {
-			return uid % this.capacity;
+	public boolean addUser(User usr) {
+		//Time Complexity
+			//Best Case O(1) - Hashcode hasn't been used.
+			//Worst Case O(n) - Every id has the same hashcode.
+		//Space Complexity O(1)
+		int uid = usr.getId();
+		if (getUser(uid) != null) return false;	//Do a check to see if usr in.
+		UserAndPoint temp = hashtable[hashFunction(uid)];
+		UserAndPoint prev = null;
+		//Set up some pointers.
+		while (temp != null) {
+			prev = temp;
+			temp = temp.getNext();
+		}
+		//If the first try wasn't null, keep cycling until it is.
+		if (prev == null) {
+			hashtable[hashFunction(uid)] = new UserAndPoint(usr);
+			//If this is the first value with this hashcode, point directly from
+			//array.
+		} else {
+			prev.gottaPointFast(new UserAndPoint(usr));
+			//If there is more than one user with this hashcode, have the previous 
+			//one point to this one.
 		}
 		
-		private User[] sortByDJ(User[] arrIn) {
-			//Merge sort is implemented here with fixed length arrays.
-			//Used arrays because I really like using arrays from working with
-			//Pascal/Delphi. Making a linked list implemented merge sort would've
-			//saved space, but computers and servers have more than enough memory.
-			//The provided parameter is an array anyways, it would require some effort
-			//just to convert that array into a linked list, then convert the linked 
-			//list back into an array. The other two sorts I was considering, heap and
-			//quicksort, kinda confuse me, so I picked this one instead.
-			//Time Complexity O(n log(n)) 
-			//https://www.khanacademy.org/computing/computer-science/algorithms/merge-sort/a/analysis-of-merge-sort
-			//Space Complexity O(n)
-			int length = arrIn.length;	//Find array length for reference.
-			if (length == 1) return arrIn;	//No need to split
-			User[] arrLeft = new User[length/2];
-			User[] arrRight = new User[length - length/2];
-			//Get the left and right side of the array, on odd numbers,
-			//arrLeft.length + 1 == arrRight.length.
-			int i = 0;
-			for (i = 0; i < length/2; i++) arrLeft[i] = arrIn[i];
-			for (i = length/2; i < length; i++) arrRight[i - length/2] = arrIn[i];
-			//Set up the left and right arrays to point at the correct users on the 
-			//left and right of the input array.
-			//System.out.println("Start Recursion");
-			arrLeft = sortByDJ(arrLeft);
-			arrRight = sortByDJ(arrRight);
-			//Recursion here
-			//System.out.println("End Recursion");
-			//Time to do some merging.
-			User[] retArray = new User[length];
-			i = 0; //Loop counter for left array.
-			int j = 0; //Loop counter for right array.
-			int k = 0; //Loop counter for return array.
-			//System.out.println("Array Lengths: L=" + arrLeft.length + " R=" + arrRight.length);
-			while (i < arrLeft.length && j < arrRight.length) {
-				//System.out.println("Nums: i=" + i + " j=" + j + " k=" + k);
-				//System.out.println("Compare=" + arrLeft[i].getDateJoined().compareTo(arrRight[j].getDateJoined()));
-				if(arrLeft[i].getDateJoined().compareTo(arrRight[j].getDateJoined()) < 0) {
-					retArray[k] = arrLeft[i];
-					//System.out.println("AddL");
-					i++;
-				} else {
-					retArray[k] = arrRight[j];
-					//System.out.println("AddR");
+		this.userCount++;	//Increment users.
+		return true;
+	}
+
+	public User getUser(int uid) {
+		//Time Complexity
+			//Best Case O(1) - Hashcode used once/is unused.
+			//Worst Case O(n) - Every id has the same hashcode.
+		//Space Complexity O(1)
+		UserAndPoint check = hashtable[hashFunction(uid)];
+		while (check != null) {
+			if (check.getCurrent().getId() == uid) return check.getCurrent();
+			check = check.getNext();
+		}
+		return null;
+	}
+
+	public User[] getUsers() {
+		//Time Complexity
+			//Best Case O(n) - No more than one ID to every hashcode.
+			//Worst Case O(n^2) - Every id has the same hashcode=capcity-1.
+		//Space Complexity O(n)
+		if (userCount == 0) return null;
+		User[] retArray = new User[userCount];
+		UserAndPoint temp;
+		int j = 0; //Used for end pointer in retArray
+		for (int i = 0; i < capacity; i++) {
+			if (hashtable[i] != null) {
+				temp = hashtable[i];
+				while (temp != null) {
+					retArray[j] = temp.getCurrent();
 					j++;
+					temp = temp.getNext();
 				}
-				k++;
 			}
-			//At the end of this loop, just fill in with the other array.
-			while (i < arrLeft.length) {
+		}
+		//Sort by date joined and return the array.
+		return sortByDJ(retArray);
+	}
+
+	public User[] getUsersContaining(String query) {
+		//Time Complexity O(n)
+		//Space Complexity O(n)
+		if (userCount == 0) return null;
+		User[] arrAllUsers = getUsers();
+		User[] retArray;
+		boolean[] arrMatch = new boolean[arrAllUsers.length]; 
+		//Indexes match with the user indexes in arrAllUsers, so if true in this
+		//array, then it means that user matches the string check.
+		//This round about way is required so we can find the length of the 
+		//return value array, then actually assign the object references without
+		//having to do another set of string checks.
+		//In why I'm using array.length instead of userCount, I just like the look
+		//of it better and helps me understand that I'm specifically linking these
+		//two arrays.
+		int count = 0; //Counter for the number of matches found.
+		int i = 0;
+		for (i = 0; i < arrMatch.length; i++) {
+			arrMatch[i] = arrAllUsers[i].getName().toLowerCase().contains(query.toLowerCase());
+			//Compare the lower case versions of the name and query. If query is
+			//or is part of the current user name, then that index has it's value 
+			//set to true.
+			if (arrMatch[i]) count++;
+		}
+		if (count == 0) return null;
+		retArray = new User[count];
+		int j = 0; //Iterator through retArray.
+		for (i = 0; i < arrMatch.length; i++)
+			if (arrMatch[i]) {
+				retArray[j] = arrAllUsers[i];
+				j++;
+			}
+		//No need to resort the array as we've kept the users in date order, just
+		//shuffled up to fill the gaps.
+		return retArray;
+	}
+
+	public User[] getUsersJoinedBefore(Date dateBefore) {
+		if (userCount == 0) return null;
+		User[] arrAllUsers = getUsers();
+		Integer count = 0;
+		//while (arrAllUsers[count].getDateJoined().compareTo(dateBefore) < 0) count++;
+		//Keep looking through the array until we find the date joined which is
+		//equal to or later than the date provided.
+		count = binSearchDate(arrAllUsers, dateBefore, 0, arrAllUsers.length - 1);
+		if (count == null) return null;
+		count += 1;
+		User[] retArray = new User[count];
+		for (int i = 0; i < count; i++) retArray[i] = arrAllUsers[i];
+		//We simply want to truncate the array of the latter parts which contains
+		//users registered after or on the specified date.
+		//As a result sorting isn't needed.
+		return retArray;
+	}
+	
+	private int hashFunction(int uid) {
+		return uid % this.capacity;
+	}
+	
+	private Integer binSearchDate(User[] users, Date target, int left, int right) {
+		//Binary search through an array of users sorted by date.
+		//Returns the index of the date before and after target.
+		if (left == right) return left;
+		int mid = (left+right)/2;
+		int comp = users[mid].getDateJoined().compareTo(target);
+		if (comp < 0) return binSearchDate(users, target, mid+1, right);
+		if (comp > 0) return binSearchDate(users, target, left, mid-1);
+		if (comp == 0) return mid;
+		return null; //Return null if we get here, something has gone wrong.
+	}
+	
+	private User[] sortByDJ(User[] arrIn) {
+		//Merge sort is implemented here with fixed length arrays.
+		//Used arrays because I really like using arrays from working with
+		//Pascal/Delphi. Making a linked list implemented merge sort would've
+		//saved space, but computers and servers have more than enough memory.
+		//The provided parameter is an array anyways, it would require some effort
+		//just to convert that array into a linked list, then convert the linked 
+		//list back into an array. The other two sorts I was considering, heap and
+		//quicksort, kinda confuse me, so I picked this one instead.
+		//Time Complexity O(n log(n)) 
+		//https://www.khanacademy.org/computing/computer-science/algorithms/merge-sort/a/analysis-of-merge-sort
+		//Space Complexity O(n)
+		int length = arrIn.length;	//Find array length for reference.
+		if (length == 1) return arrIn;	//No need to split
+		User[] arrLeft = new User[length/2];
+		User[] arrRight = new User[length - length/2];
+		//Get the left and right side of the array, on odd numbers,
+		//arrLeft.length + 1 == arrRight.length.
+		int i = 0;
+		for (i = 0; i < length/2; i++) arrLeft[i] = arrIn[i];
+		for (i = length/2; i < length; i++) arrRight[i - length/2] = arrIn[i];
+		//Set up the left and right arrays to point at the correct users on the 
+		//left and right of the input array.
+		//System.out.println("Start Recursion");
+		arrLeft = sortByDJ(arrLeft);
+		arrRight = sortByDJ(arrRight);
+		//Recursion here
+		//System.out.println("End Recursion");
+		//Time to do some merging.
+		User[] retArray = new User[length];
+		i = 0; //Loop counter for left array.
+		int j = 0; //Loop counter for right array.
+		int k = 0; //Loop counter for return array.
+		//System.out.println("Array Lengths: L=" + arrLeft.length + " R=" + arrRight.length);
+		while (i < arrLeft.length && j < arrRight.length) {
+			//System.out.println("Nums: i=" + i + " j=" + j + " k=" + k);
+			//System.out.println("Compare=" + arrLeft[i].getDateJoined().compareTo(arrRight[j].getDateJoined()));
+			if(arrLeft[i].getDateJoined().compareTo(arrRight[j].getDateJoined()) > 0) {
 				retArray[k] = arrLeft[i];
 				//System.out.println("AddL");
 				i++;
-				k++;
-			}
-			
-			while (j < arrRight.length) {
+			} else {
 				retArray[k] = arrRight[j];
 				//System.out.println("AddR");
 				j++;
-				k++;
 			}
-			
-			arrLeft = arrRight = null; //Dereference our l/r arrays.
-			//System.out.println("End Merge");
-			return retArray;
-			//Done with the merging, let's bring it back up.
+			k++;
 		}
+		//At the end of this loop, just fill in with the other array.
+		while (i < arrLeft.length) {
+			retArray[k] = arrLeft[i];
+			//System.out.println("AddL");
+			i++;
+			k++;
+		}
+		
+		while (j < arrRight.length) {
+			retArray[k] = arrRight[j];
+			//System.out.println("AddR");
+			j++;
+			k++;
+		}
+		
+		arrLeft = arrRight = null; //Dereference our l/r arrays.
+		//System.out.println("End Merge");
+		return retArray;
+		//Done with the merging, let's bring it back up.
+	}
 }
--- tests/WeetTests.java
+++ tests/WeetTests.java
@@ -2,113 +2,134 @@ import java.util.Date;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.text.ParseException;
+import java.util.Random;
 
 
+import uk.ac.warwick.java.cs126.models.User;
 import uk.ac.warwick.java.cs126.services.IWeetStore;
 import uk.ac.warwick.java.cs126.services.WeetStore;
 import uk.ac.warwick.java.cs126.models.Weet;
 
-class WeetTests {
-    
-
-    /*
-     * Tests the ability to add a weet
-     * @return Returns true is the test passed, false is it failed
-     */
-    protected boolean testAddWeet() 
-    {
-        // Create new Weet Store
-        IWeetStore weetStore = new WeetStore();
-
-        // Generate Weet to add
-        Weet weet = new Weet(1, 1, "Hello World!", createDate("02/11/2012 23:11"));
-
-        // Issue the command, suitably storing the return value
-        boolean result = weetStore.addWeet( weet );
-
-        // Check the return value for the expected result
-        if (result == true) // Here we expect addWeet to return true if it worked
-        {
-            return true;
-        }
-        else 
-        { 
-            return false;
-        }
-    }
-
-    /*
-     * Tests the ability to retrieve a weet
-     * @return Returns true is the test passed, false is it failed
-     */
-    protected boolean testGetWeetSuccess()
-    {
-        // Create new Weet Store
-        IWeetStore weetStore = new WeetStore();
-
-        // Generate Weet to add
-        int weetId = 1;
-        Weet weet = new Weet(weetId, 1, "Hello World!", createDate("02/11/2012 23:11"));
-
-        // Add that weet
-        weetStore.addWeet( weet );
-
-        // Issue the command, suitably storing the return value
-        Weet returnedWeet = weetStore.getWeet(weetId);
-        
-        // Check the return value for the expected result
-        if (returnedWeet == weet) // We expect the Weet we get back to be the same as the one put in 
-        {
-            return true;
-        }
-        else 
-        { 
-            return false;
-        }
-
-    }
-
-    /*
-     * Tests what happens when an invalid Weet is searched for 
-     * @return Returns true is the test passed, false is it failed
-     */
-    protected boolean testGetWeetFail()
-    {
-        // Create new Weet Store
-        IWeetStore weetStore = new WeetStore();
-
-        int weetId = 2;
-        // Try retrieve a Weet that won't be in the store 
-        Weet returnedWeet = weetStore.getWeet(weetId);
-        
-        // Check the return value for the expected result
-        if (returnedWeet == null) // We expect it to return null 
-        {
-            return true;
-        }
-        else 
-        { 
-            return false;
-        }
-    }
-
-    /*
-     * Returns a Date based on the input string
-     * @param inputString Takes a string of form dd/MM/yy hour:minute. Example: 01/03/12 18:00
-     * @return Returns the create Date
-     */
-    private Date createDate(String inputString) // This method is useful for creating dates quickly, where can be good for testing
-    {
-        try {
-            // Take input string and create date
-            DateFormat dateFormatter = new SimpleDateFormat("dd/MM/yy H:m");
-            return dateFormatter.parse(inputString);
-        }
-        catch (ParseException pe) 
-        {
-            // Bad input string
-            System.out.println("Couldn't parse " + inputString);
-            return new Date();
-        }
-    }
+public class WeetTests {
+    private Random random;
+		//Used to generate random numbers.
+		private WeetStore weets;
+		private int randInt;
+		
+		public WeetTests() {
+			weets = new UserStore();
+			random = new Random();
+			randInt = 0;
+		}
+		public int testAddWeet() 
+		{
+			//Add a single weet with random id numbers.
+			//Returns the ID number used if successful, else return -1.
+			randInt = random.nextInt(1000000);
+			User temp = new User("Just A Dude" + randInt % 10,randInt, new Date((long) random.nextInt()));
+			System.out.println(userToString(temp));
+			return ((users.addUser(temp)) ? randInt : -1);
+		}
+		
+		public boolean testAddWithID(int uid) {
+			randInt = random.nextInt(1000000);
+			User temp = new User("Just A Dude" + randInt % 10, uid, new Date((long) random.nextInt()));
+			return users.addUser(temp);
+		}
+		
+		public User testGetUser(int uid) {
+			//Return null if user not found.
+			return users.getUser(uid);
+		}
+		
+		public User[] testGetUsers() {
+			return users.getUsers();
+		}
+		
+		public User[] testGetUsersContaining(String q) {
+			return users.getUsersContaining(q);
+		}
+		
+		public User[] testGetUsersJoinedBeforeDate(Date date) {
+			return users.getUsersJoinedBefore(date);
+		}
+		
+		public String arrUserToString(User[] arr) {
+			if (arr == null) return "NO ARRAY";
+			String retString = "[\n";
+			for (int i = 0; i < arr.length; i++) {
+				retString += userToString(arr[i]) + "\n";
+			}
+			retString += "]";
+			return retString;
+		}
+		
+		public String userToString(User u) {
+			if (u == null) return "NO USER";
+			return "(" + u.getName() + " " + u.getId() + " " + u.getPrettyDateJoined() + ")";
+		}
+		
+		public static void main(String[] args) {
+		UserTests test = new UserTests();
+		
+		System.out.println("Start UserTests");
+		//Initialise Stuff
+		final int idLength = 10;
+		User[] users;
+		int[] ids = new int[idLength];
+		int i = 0;
+		int j = 0;
+		System.out.println();
+		System.out.println("Add " + idLength + " Users");
+		//Add ten users to the userStore with random IDs.
+		while (i < idLength) {
+			j = test.testAddUser();
+			if (j > -1) {
+				ids[i] = j;
+				System.out.println("Added User to Array, ID = " + j);
+				i++;
+			} else {
+				System.out.println("Collision of IDs, user not added, ID = " + j);
+			}
+		}
+		System.out.println();
+		System.out.println("Check collisions with " + idLength + " Users");
+		//Explicit Collision Tests.
+		for (i = 0; i < idLength; i++)	
+			System.out.println("Success in adding while ID exists: " + test.testAddWithID(ids[i]));
+		
+		System.out.println();
+		System.out.println("Check getting by ID");
+		//Check we can access all ten users by ID.
+		for (i = 0; i < idLength; i++)
+			System.out.println("Success in getting: " + (test.testGetUser(ids[i]) != null));
+		
+		//Extract all users added so far.
+		users = test.testGetUsers();
+		System.out.println();
+		System.out.println("Get All Users Test: " + (users != null));
+		System.out.println(test.arrUserToString(users));
+		//Array of users will be sorted by date, therefore the middle value should
+		//bisect the list in terms of dates. Used later.
+		Date dateTemp = users[idLength / 2].getDateJoined();
+		
+		//Check for string containing.
+		System.out.println();
+		System.out.println("Check generic name part i.e. query = 'Just'");
+		users = test.testGetUsersContaining("Just");
+		System.out.println(test.arrUserToString(users));
+		System.out.println();
+		System.out.println("Check against an ID number's LSD = " + ids[idLength / 2] % 10);
+		users = test.testGetUsersContaining("" + (ids[idLength / 2] % 10));
+		System.out.println(test.arrUserToString(users));
+		
+		//Check for joined before date.
+		System.out.println();
+		System.out.println("Check before date: " + dateTemp.toString());
+		System.out.println(test.arrUserToString(test.testGetUsersJoinedBeforeDate(dateTemp)));
+		
+		System.out.println("End UserStore Tests");
+		
+		
+	}
 }
